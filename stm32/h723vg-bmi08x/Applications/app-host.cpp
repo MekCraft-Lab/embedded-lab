4111e72ac397ae94d5085d3770663a76011130fb
MekCraftLi <Jiahao.Mek.Li@outlook.com>
2025-11-24 03:04:05 +0800

    feat: 添加串口输出功能

---
+++ stm32/h723vg-bmi08x/Applications/app-host.cpp
@@ -0,0 +1,129 @@
+/**
+ *******************************************************************************
+ * @file    app-host.cpp.cpp
+ * @brief   简要描述
+ *******************************************************************************
+ * @attention
+ *
+ * none
+ *
+ *******************************************************************************
+ * @note
+ *
+ * none
+ *
+ *******************************************************************************
+ * @author  MekLi
+ * @date    2025/11/24
+ * @version 1.0
+ *******************************************************************************
+ */
+
+
+
+
+/* ------- define ----------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/* ------- include ---------------------------------------------------------------------------------------------------*/
+
+
+
+/* I. header */
+
+#include "app-host.h"
+
+/* II. other application */
+
+
+/* III. standard lib */
+
+
+
+
+/* ------- class prototypes-----------------------------------------------------------------------------------------*/
+
+
+
+
+/* ------- macro -----------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/* ------- variables -------------------------------------------------------------------------------------------------*/
+
+
+
+
+
+/* ------- application attribute -------------------------------------------------------------------------------------*/
+
+#define APPLICATION_ENABLE     true
+
+#define APPLICATION_NAME       "Host"
+
+#define APPLICATION_STACK_SIZE 512
+
+#define APPLICATION_PRIORITY   4
+
+static StackType_t appStack[APPLICATION_STACK_SIZE];
+
+static HostApp hostApp;
+
+
+
+
+/* ------- message interface attribute -------------------------------------------------------------------------------*/
+
+
+
+
+
+
+/* ------- function prototypes ---------------------------------------------------------------------------------------*/
+
+
+
+
+
+/* ------- function implement ----------------------------------------------------------------------------------------*/
+
+
+HostApp::HostApp()
+    : StaticAppBase(APPLICATION_ENABLE, APPLICATION_NAME, APPLICATION_STACK_SIZE,  appStack, APPLICATION_PRIORITY, 0, nullptr){
+}
+
+
+HostApp& HostApp::instance() {
+    return hostApp;
+}
+
+
+void HostApp::init() {
+    /* driver object initialize */
+}
+
+
+void HostApp::run() {
+ 
+}
+
+
+
+uint8_t HostApp::rxMsg(void* msg, uint16_t size) {
+
+    return 0;
+}
+
+uint8_t HostApp::rxMsg(void* msg, uint16_t size, TickType_t timeout) {
+
+    return 0;
+}
+
+
+
